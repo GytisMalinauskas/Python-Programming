5bce513001ba24376f9645fee3c581f418f45317
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-20 15:03:02 +0000

    Aug 20, 2026, 6:03 PM

--- simple_linked_list/simple_linked_list.py
+++ simple_linked_list/simple_linked_list.py
@@ -1,7 +1,7 @@
 """This is a module of linked list"""
 
 """ 
-    (singly linked list)Linked list contains data and the reference to next node,
+    Linked list contains data and the reference to next node,
     and null value at the end .
     All you need to know is the location of the head.
     If you have previous node reference, then you can also
