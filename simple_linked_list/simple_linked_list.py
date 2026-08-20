810c844f6538b4626a64ced9e00ae25761320f28
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-20 15:02:48 +0000

    Aug 20, 2026, 6:02 PM

--- simple_linked_list/simple_linked_list.py
+++ simple_linked_list/simple_linked_list.py
@@ -2,7 +2,7 @@
 
 """ 
     Linked list contains data and the reference to next node,
-    and null value at the end(singly linked list).
+    and null value at the end (singly linked list).
     All you need to know is the location of the head.
     If you have previous node reference, then you can also
     traverse back (doubly linked list).
