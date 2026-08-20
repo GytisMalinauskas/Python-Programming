37534ffc4bc942f4f9c604697c8994033ef81e57
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-20 14:58:44 +0000

    Aug 20, 2026, 5:58 PM

--- simple_linked_list/simple_linked_list.py
+++ simple_linked_list/simple_linked_list.py
@@ -3,7 +3,8 @@
 """ 
     Linked list contains data and the reference to next node
     all you need to know is the location of the head.
-    If you have 2 references - next and previous - 
+    If you have 2 references - next and previous - then you can also
+    traverse back
 """
 
 class EmptyListException(Exception):
