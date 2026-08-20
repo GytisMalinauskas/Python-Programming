59ca13af72022920ce84468fb34540c02e9cbdd1
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-20 14:58:24 +0000

    Aug 20, 2026, 5:58 PM

--- simple_linked_list/simple_linked_list.py
+++ simple_linked_list/simple_linked_list.py
@@ -3,7 +3,7 @@
 """ 
     Linked list contains data and the reference to next node
     all you need to know is the location of the head.
-    If you have 2 reference
+    If you have 2 references
 """
 
 class EmptyListException(Exception):
