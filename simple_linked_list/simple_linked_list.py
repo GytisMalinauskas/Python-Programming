7f4be4b1c546be3279f0c00f921cabeeb568e3ff
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-20 15:05:10 +0000

    Aug 20, 2026, 6:05 PM

--- simple_linked_list/simple_linked_list.py
+++ simple_linked_list/simple_linked_list.py
@@ -6,7 +6,7 @@
     Doubly linked list is the same but you also have previous node
     reference and then you can traverse back.
     Circular linked list does not end with null, its last value is
-    considered a tails and 
+    considered a tails and node after is
 """
 
 class EmptyListException(Exception):
