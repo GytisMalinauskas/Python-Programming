151bf591441b4ab8b2d1cf80126cfc7df5a70bdc
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-20 15:03:41 +0000

    Aug 20, 2026, 6:03 PM

--- simple_linked_list/simple_linked_list.py
+++ simple_linked_list/simple_linked_list.py
@@ -3,7 +3,7 @@
 """ 
     Singly linked list contains data and the reference to next node,
     and null value at the end and all you need to know is the location of the head.
-    Doubly linked list is the same you have previous node reference, then you can also
+    Doubly linked list is the same but you also have previous node reference, then you can also
     traverse back .
     Circular linked list does not end with nul
 """
