629845f2621673c4ac97ecf967891ed2c0097d37
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-20 15:02:41 +0000

    Aug 20, 2026, 6:02 PM

--- simple_linked_list/simple_linked_list.py
+++ simple_linked_list/simple_linked_list.py
@@ -1,7 +1,8 @@
 """This is a module of linked list"""
 
 """ 
-    Linked list contains data and the reference to next node, and null value at the end.
+    Linked list contains data and the reference to next node,
+    and null value at the end.
     All you need to know is the location of the head(singly linked list).
     If you have previous node reference, then you can also
     traverse back (doubly linked list).
