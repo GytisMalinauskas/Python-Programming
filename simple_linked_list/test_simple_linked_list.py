3b177b2e5d8a342c2f54ef7ee4d982e5f04d2846
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-21 08:22:38 +0000

    Aug 21, 2026, 11:22 AM

--- simple_linked_list/test_simple_linked_list.py
+++ simple_linked_list/test_simple_linked_list.py
@@ -1,23 +1,25 @@
 from simple_linked_list import LinkedList
 
 def main():
-    linked_list = LinkedList()
-    linked_list.push(1)
-    linked_list.push(2)
-    linked_list.push(0)
-    linked_list.push(3)
-    current = linked_list.list_head
+    # linked_list = LinkedList()
+    # linked_list.push(1)
+    # linked_list.push(2)
+    # linked_list.push(0)
+    # linked_list.push(3)
+    # current = linked_list.list_head
     
-    while current:
-        print(current.value())
-        current = current.next_node
+    # while current:
+    #     print(current.value())
+    #     current = current.next_node
     
-    print("pop: ", linked_list.pop())
-    current = linked_list.list_head
-    while current:
-        print(current.value())
-        current = current.next_node
-    print("lenght: ", len(linked_list))
+    # print("pop: ", linked_list.pop())
+    # current = linked_list.list_head
+    # while current:
+    #     print(current.value())
+    #     current = current.next_node
+    # print("lenght: ", len(linked_list))
+    ll = LinkedList([1,2,3])
+    print(list(ll))
     
 if __name__ == "__main__":
     main()
