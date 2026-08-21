565e8fcd3adb5a5d900904aae4195af46fc9bdc0
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-21 07:24:15 +0000

    Aug 21, 2026, 10:24 AM

--- simple_linked_list/test_simple_linked_list.py
+++ simple_linked_list/test_simple_linked_list.py
@@ -2,7 +2,11 @@ from simple_linked_list import LinkedList
 
 def main():
     linked_list = LinkedList()
-    linked_list.push(3)
+    linked_list.push(1)
+    linked_list.push(2)
+    current = linked_list.list_head
+    print(current.value())
+    
     
 if __name__ == "__main__":
     main()
