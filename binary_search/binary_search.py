84946432f765b488613f7cfc2421a505b3d6b890
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-22 12:48:00 +0000

    Aug 22, 2026, 3:48 PM

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,10 +1,9 @@
 ERROR_MESSAGE = "value not in array"
 
 def find(search_list, value):
-    search_list = search_list.sort()
-    if value < search_list[0] or value > search_list[search_list.count() - 1]:
+    if value < search_list[0] or value > search_list[len(search_list) - 1]:
         raise ValueError(ERROR_MESSAGE)
-    index = search_list.count() - 1
+    index = len(search_list) - 1
     rotation_count = 0
     while index > 0:
         if index % 2 != 0:
