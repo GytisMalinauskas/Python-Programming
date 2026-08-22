ad3d9dfc11270fb46cdf86ae2beda1a68dda0b09
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-22 14:40:41 +0000

    Aug 22, 2026, 5:40 PM

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -8,7 +8,7 @@ def find(search_list, value):
     while True:
         if value < search_list[lower_bound] or value > search_list[upper_bound]:
             raise ValueError(ERROR_MESSAGE)
-        index = (upper_bound + lower_bound) / 2
+        index = (upper_bound + lower_bound) // 2
         if index % 2 == 0:
             index = int(index) 
         else:
