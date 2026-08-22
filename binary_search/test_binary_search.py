5d5681a6ffb88251fa3ebaea5aa2eea9bf43ff3b
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-22 13:31:27 +0000

    Aug 22, 2026, 4:31 PM

--- binary_search/test_binary_search.py
+++ binary_search/test_binary_search.py
@@ -2,8 +2,8 @@ from binary_search import find
 
 def main():
     # print(find([1,2,3,7,9,20,40], 2))
-    print(find([1,2,3,7,9,20,40], 0))
-    print(find([1,2,3,7,9,20,40], 50))
+    # print(find([1,2,3,7,9,20,40], 0))
+    # print(find([1,2,3,7,9,20,40], 50))
     print(find([1,2,3,7,9,20,40], 5))
 
 if __name__ == "__main__":
