9322e12231d55cd3b0d1dd6cad4650ab1100f740
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-23 07:52:56 +0000

    Aug 23, 2026, 10:52 AM

--- circular_buffer/test_circular_buffer.py
+++ circular_buffer/test_circular_buffer.py
@@ -3,7 +3,7 @@ from circular_buffer import CircularBuffer
 def main():
     circular_buffer = CircularBuffer(2)
     circular_buffer.write(1)
-    print(circular_buffer.read(1))
+    print(circular_buffer.read())
 
 if __name__ == "__main__":
     main()
