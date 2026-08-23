1fd3e1ee1e07263127c1be280e659f7b8e58e3c1
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-23 11:30:46 +0000

    Aug 23, 2026, 2:30 PM

--- circular_buffer/test_circular_buffer.py
+++ circular_buffer/test_circular_buffer.py
@@ -3,6 +3,7 @@ from circular_buffer import CircularBuffer
 def main():
     circular_buffer = CircularBuffer(2)
     circular_buffer.write(1)
+    circular_buffer.write(3)
     circular_buffer.overwrite(2)
     
 if __name__ == "__main__":
