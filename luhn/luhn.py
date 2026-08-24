327bf4d23986eb718db4d073415c63ebcbc4d5aa
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-24 15:40:20 +0000

    Aug 24, 2026, 6:40 PM

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -3,11 +3,9 @@ class Luhn:
         self.card_num = card_num
 
     def valid(self):
-        self.card_num
         if len(self.card_num.strip()) < 2:
             return False
         joined_card_num = "".join(self.card_num.split(" "))
         if not joined_card_num.isdecimal():
             return False
         
-            
--- luhn/test_luhn.py
+++ luhn/test_luhn.py
@@ -3,7 +3,7 @@ from luhn import Luhn
 def main():
     print(Luhn("098$ 3434").valid())
     print(Luhn("098 3434").valid())
-    print(Luhn("098 3434").valid())
+    print(Luhn("9").valid())
     
 if __name__ == "__main__":
     main()
