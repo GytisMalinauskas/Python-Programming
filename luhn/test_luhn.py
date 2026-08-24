d964b39a642fa027293d2f8e982bb0499b58356b
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-24 15:38:46 +0000

    Aug 24, 2026, 6:38 PM

--- luhn/test_luhn.py
+++ luhn/test_luhn.py
@@ -2,7 +2,8 @@ from luhn import Luhn
 
 def main():
     print(Luhn("098$ 3434").valid())
-    
+    print(Luhn("098 3434").valid())
+    print(Luhn("098 3434").valid())
     
 if __name__ == "__main__":
     main()
