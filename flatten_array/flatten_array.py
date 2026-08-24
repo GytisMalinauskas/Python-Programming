6f586e18c3409396458a8c766f9f268107d21aa8
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-24 12:42:42 +0000

    Aug 24, 2026, 3:42 PM

--- flatten_array/flatten_array.py
+++ flatten_array/flatten_array.py
@@ -2,4 +2,12 @@
 
 def flatten(iterable):
     """Flattens the given list into one"""
-    pass
+    flattened_list=[]
+    for value in iterable:
+        if value is int:
+            flattened_list.append(value)
+        if value is None:
+            continue
+        if value is list:
+            flattened_list.append(flatten(value))
+    return flattened_list
