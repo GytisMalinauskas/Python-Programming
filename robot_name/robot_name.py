bdd39ebaf43e14a328040e1236659569d3f7025e
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-25 10:30:16 +0000

    Aug 25, 2026, 1:30 PM

--- robot_name/robot_name.py
+++ robot_name/robot_name.py
@@ -21,7 +21,8 @@ class Robot:
         remove_name = self._name
         while self._name in names_in_use:
             self._name = generate_name()
-        names_in_use.pop(remove_name)
+        index = names_in_use.index(remove_name)
+        names_in_use.pop(index)
         names_in_use.append(self._name)
     
 def generate_name():
