fedcb60aa660ae24ce3d8a3d8a0cbb0f532db411
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-25 10:05:49 +0000

    Aug 25, 2026, 1:05 PM

--- robot_name/robot_name.py
+++ robot_name/robot_name.py
@@ -1,16 +1,26 @@
+
+"""A module for Robot name generation"""
 import random
 import string
 
 class Robot:
+    """Initializes robot with a random generated name"""
     def __init__(self):
-        self._name = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
+        self._name = generate_name()
         self.used_names = []
+        
     @property
     def name(self):
+        """Returns name value"""
         return self._name
 
     def reset(self):
+        """Resets name value"""
         self.used_names.append(self._name)
-        self._name = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
-        if self._name in self.used_names:
-            self._name = self._name = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
+        self._name = generate_name()
+        while self._name not in self.used_names:
+            self._name = generate_name()
+    
+def generate_name():
+    """Generates robot name"""
+    return random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
