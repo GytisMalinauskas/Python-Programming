785d6f51076a172d4b2e652b26b1b9467adcb93e
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-25 09:53:43 +0000

    Aug 25, 2026, 12:53 PM

--- robot_name/robot_name.py
+++ robot_name/robot_name.py
@@ -3,11 +3,11 @@ import string
 
 class Robot:
     def __init__(self):
-        self._name = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + str(random.randrange(100, 999, 1))
+        self._name = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
 
     @property
     def name(self):
         return self._name
 
     def reset(self):
-        self._name = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + str(random.randrange(100, 999, 1))
+        self._name = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
