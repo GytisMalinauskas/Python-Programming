05b653124b1c1b18a4d22b6f13f757fe049a39eb
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-25 09:56:02 +0000

    Aug 25, 2026, 12:56 PM

--- robot_name/robot_name.py
+++ robot_name/robot_name.py
@@ -4,10 +4,13 @@ import string
 class Robot:
     def __init__(self):
         self._name = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
-
+        self.used_names = []
     @property
     def name(self):
         return self._name
 
     def reset(self):
+        self.used_names.append(self._name)
         self._name = random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + str(random.randrange(10)) + str(random.randrange(10)) + str(random.randrange(10))
+        if self._name in self.used_names:
+            self._name
