3b53a4ec7823eb21f655b8d00c834266d3786239
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-25 09:29:59 +0000

    Aug 25, 2026, 12:29 PM

--- robot_name/robot_name.py
+++ robot_name/robot_name.py
@@ -9,3 +9,7 @@ class Robot:
     def name(self):
         self._name = (random.choice(string.ascii_uppercase) * 2) + str(random.randrange(100, 999, 1))
         return self._name
+
+    def reset(self):
+        self._name = ""
+        self.name
--- robot_name/test_robot_name.py
+++ robot_name/test_robot_name.py
@@ -3,6 +3,8 @@ from robot_name import Robot
 def main():
     robot = Robot()
     print(robot.name)
+    robot.reset()
+    print(robot.name)
     
 if __name__ == "__main__":
     main()
