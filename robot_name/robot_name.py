ad99ebe17015847706a05eda242c2a3446d7a4f2
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-25 09:24:33 +0000

    Aug 25, 2026, 12:24 PM

--- robot_name/robot_name.py
+++ robot_name/robot_name.py
@@ -1,9 +1,10 @@
 import random
 
+
 class Robot:
     def __init__(self):
         self._name = ""
 
     @property
     def name(self):
-        self._name = (random.choice(str.ascii_uppercase()) * 2) + str(random.randrange(100, 999, 1))
+        self._name = (random.choice(str.ascii_uppercase) * 2) + str(random.randrange(100, 999, 1))
