a2947bba2da4e8ec99001c2bd5e8165d13a9828f
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-26 15:54:46 +0000

    Aug 26, 2026, 6:54 PM

--- clock/clock.py
+++ clock/clock.py
@@ -2,14 +2,14 @@
 
 class Clock:
     def __init__(self, hour, minute):
-        self.hour = int(hour + (minute / 60))
-        self.minute = int(minute % 60)
+        self.hour = hour
+        self.minute = minute
 
     def __repr__(self):
         return f"Clock({self.hour}, {self.minute})"
 
     def __str__(self):
-        return f"{self.hour}:{self.minute}"
+        return f"{format_hour(self.hour, self.minute):.2f}:{format_minute(self.minute)}"
 
     def __eq__(self, other):
         pass
@@ -19,3 +19,9 @@ class Clock:
 
     def __sub__(self, minutes):
         pass
+
+def format_hour(hour, minute):
+    return int(hour + (minute / 60))
+
+def format_minute(minute):
+    return int(minute % 60)
