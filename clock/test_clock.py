002e09ae41f5725fb6e3f8ba7f246ea49f2fb267
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-26 09:02:36 +0000

    Aug 26, 2026, 12:02 PM

--- clock/test_clock.py
+++ clock/test_clock.py
@@ -2,8 +2,6 @@ from clock import Clock
 
 def main():
     clock = Clock(11, 30)
-    print(str(clock))
-    print(eval(str(clock)))
-    
+    print(str(clock))    
 if __name__ == "__main__":
     main()
