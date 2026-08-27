faef83657cdc87a795c59f34a1a5af6b9c45356a
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-27 13:29:58 +0000

    Aug 27, 2026, 4:29 PM

--- spiral_matrix/spiral_matrix.py
+++ spiral_matrix/spiral_matrix.py
@@ -1,2 +1,9 @@
+"""A module for filling spiral matrix of given size with natural numbers"""
+
 def spiral_matrix(size):
-    pass
+    top = 0
+    bottom = 0
+    right = 0
+    left = 0
+    
+    
---
+++ spiral_matrix/test_spiral_matrix.py
@@ -0,0 +1,8 @@
+from spiral_matrix import spiral_matrix
+
+def main():
+    print(spiral_matrix(1))
+    print(spiral_matrix(2))
+    print(spiral_matrix(3))
+    print(spiral_matrix(4))
+    print(spiral_matrix(5))
