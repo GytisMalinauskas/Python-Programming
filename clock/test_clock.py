3f8fd1bd2b2494ad1e4088f4e273757ce088f4f5
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-27 08:03:57 +0000

    Aug 27, 2026, 11:03 AM

--- clock/test_clock.py
+++ clock/test_clock.py
@@ -8,7 +8,7 @@ def main():
     print(repr(clock + 20))
     print(str(clock - 20))
     print(repr(clock - 20))
-    clock2 = Clock(11, 30)
+    clock2 = Clock(35, 30)
     clock3 = Clock(11, 40)
     print(clock==clock2)
     print(clock==clock3)        
