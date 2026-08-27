0c276d159c999794b5fcc634cb8e84bc4a4134ab
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-27 14:07:58 +0000

    Aug 27, 2026, 5:07 PM

--- spiral_matrix/spiral_matrix.py
+++ spiral_matrix/spiral_matrix.py
@@ -2,11 +2,18 @@
 
 def spiral_matrix(size):
     top = 0
-    bottom = col - 1
-    right = row - 1
+    bottom = size - 1
+    right = size - 1
     left = 0
     matrix = []
+    row_index = 0
+    col_index = 0
+    value_index = 1
     
+    while bottom >= top and right >= left:
+        while row_index <= right:
+            ...
     
+    return matrix
     
     range(16)
