d8a48e3aa3e69f241387cd2250cd451a791361d9
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-27 09:07:01 +0000

    Aug 27, 2026, 12:07 PM

--- clock/test_clock.py
+++ clock/test_clock.py
@@ -1,17 +1,9 @@
 from clock import Clock
 
 def main():
-    clock = Clock(35, 30)
-    print(str(clock))
-    print(repr(clock))
-    print(str(clock + 20))
-    print(repr(clock + 20))
-    print(str(clock - 20))
-    print(repr(clock - 20))
-    clock2 = Clock(35, 30)
-    clock3 = Clock(11, 40)
+    clock = Clock(10, 37)
+    clock2 = Clock(34, 37)
     print(clock==clock2)
-    print(clock==clock3)        
     
 if __name__ == "__main__":
     main()
