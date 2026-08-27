ae8f6c77ff6a0a6c4e892592bbcd04224ba40da2
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-27 08:21:04 +0000

    Aug 27, 2026, 11:21 AM

--- clock/clock.py
+++ clock/clock.py
@@ -19,14 +19,14 @@ class Clock:
         total_minutes = self.minute + minutes + (self.hour * 60) + self.day * 1440
         self.hour = format_hour(total_minutes)
         self.minute = format_minute(total_minutes)
-        self.day = ...
+        self.day = format_day(total_minutes)
         return Clock(self.hour, self.minute)
 
     def __sub__(self, minutes):
         total_minutes = self.minute - minutes + (self.hour * 60) + self.day * 1440
         self.hour = format_hour(total_minutes)
         self.minute = format_minute(total_minutes)
-        self.day = ...
+        self.day = format_day(total_minutes)
         return Clock(self.hour, self.minute)
 
 def format_day(minute: int):
@@ -39,4 +39,4 @@ def format_hour(minute: int):
 
 def format_minute(minute: int):
     """formats minutes"""
-    return int(minute % 60)
+    return int(minute % 1440 % 60)
