f7ab11d654729b7b551f2f6fea82dcfa036ec65f
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-27 14:34:28 +0000

    Aug 27, 2026, 5:34 PM

--- spiral_matrix/spiral_matrix.py
+++ spiral_matrix/spiral_matrix.py
@@ -5,17 +5,15 @@ def spiral_matrix_f(size):
     bottom = size - 1
     right = size - 1
     left = 0
-    matrix = []
-    row_index = 0
-    col_index = 0
-    value_index = 1
+    matrix = [[0] * size for _ in range(size)]
+    # row_index = 0
+    # col_index = 0
+    # value_index = 1
     
-    while bottom >= top and right >= left and value_index <= size**2:
-        while col_index <= right:
-            matrix[top].append(value_index)
-            value_index += 1
-            col_index += 1
-        top += 1
-        return matrix
+    # while bottom >= top and right >= left and value_index <= size**2:
+    #     while col_index <= right:
+    #         ...
+    #     top += 1
+    #     return matrix
     
     return matrix
