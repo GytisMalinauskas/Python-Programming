80397386da2bab5498c11a4cedab18bfed6b00eb
GytisMalinauskas <pvardenis30@gmail.com>
2026-08-28 10:35:51 +0000

    Aug 28, 2026, 1:35 PM

--- spiral_matrix/spiral_matrix.py
+++ spiral_matrix/spiral_matrix.py
@@ -26,7 +26,7 @@ def spiral_matrix_f(size):
             row_index += 1
         right += 1
         row_index -= 1
-        col_index += 1
+        col_index -= 1
         while col_index >= left:
             matrix[row_index][col_index] = value_index
             value_index += 1
