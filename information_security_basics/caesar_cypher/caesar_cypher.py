1ca76f386dcfb244cadd2d5a2f1d4f5720e0d4fa
GytisMalinauskas <pvardenis30@gmail.com>
2026-09-10 13:57:29 +0000

    Sep 10, 2026, 4:57 PM

--- information_security_basics/caesar_cypher/caesar_cypher.py
+++ information_security_basics/caesar_cypher/caesar_cypher.py
@@ -1,17 +1,21 @@
 """A module for implementing Caesar cipher encryption and decryption"""
 # 9th line in txt file
 LT_ALPHABET = "aąbcčdeęėfghiįyjklmnoprsštuųūvzž"
-{char : i for char, i in enumerate(alphabet)}
 
 class Alphabet:
     def __init__(self, alphabet):
         self.alphabet = alphabet
+        self.enumeration = {char : i for char, i in enumerate(alphabet)}
     
     def __len__(self):
         return len(self.alphabet)
     
     def __str__(self):
         return self.alphabet
+    
+    def __iter__(self):
+        for value, index in self.enumeration:
+            return value, index
     
 
 def main():
@@ -20,7 +24,9 @@ def main():
     print(caesar_cypher(n, alphabet))
 
 def caesar_cypher(n: int,  alphabet: Alphabet):
-    print(n, len(alphabet), alphabet)
+    # print(n, len(alphabet), alphabet)
+    for value, index in alphabet:
+        print(value, index)
 
 if __name__ == "__main__":
     main()
