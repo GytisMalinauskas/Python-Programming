1e565596065a337cd5c2573bf3213085f2288f96
GytisMalinauskas <pvardenis30@gmail.com>
2026-09-10 12:59:26 +0000

    Sep 10, 2026, 3:59 PM

--- information_security_basics/caesar_cypher/caesar_cypher.py
+++ information_security_basics/caesar_cypher/caesar_cypher.py
@@ -1,2 +1,4 @@
 """A module for implementing Caesar cipher encryption and decryption"""
 
+ALPHABET = "aąbcčdeęėfghiįyjklmnoprsštuųūvzž"
+ALPHABET_DICT = {char: i for char, i in enumerate(ALPHABET)}
