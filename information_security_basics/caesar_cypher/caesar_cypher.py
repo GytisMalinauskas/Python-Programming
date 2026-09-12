12bda3af75ee76ed4d43dcd3f6e03babbfb67693
GytisMalinauskas <pvardenis30@gmail.com>
2026-09-12 09:13:57 +0000

    Sep 12, 2026, 12:13 PM

--- information_security_basics/caesar_cypher/caesar_cypher.py
+++ information_security_basics/caesar_cypher/caesar_cypher.py
@@ -24,7 +24,7 @@ class Alphabet:
         return self.alphabet
     
     def __getitem__(self, key):
-        return self.alphabet_dictionary[key]
+        return self.alphabet_dictionary
     
     def __iter__(self):
         """Yields value and index from initialized dictionary when iterating"""
