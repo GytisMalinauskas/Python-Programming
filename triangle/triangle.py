eb675f647bdc6a2c6d27a08cc3fc35c789a95474
GytisMalinauskas <pvardenis30@gmail.com>
2026-09-15 13:33:10 +0000

    Sep 15, 2026, 4:33 PM

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -14,8 +14,13 @@ def scalene(sides):
     """Checks if the triangle is scalene and returns boolean value"""
     if len(sides) > 3:
         return False
-    
 
+def is_triangle(sides):
+    """Determines if a shape is a trangle"""
+    for side in sides:
+        if not side > 0:
+            return False
+        
 def main():
     test1 = [1,1,1]
     test2 = [3,4,4]
@@ -23,7 +28,7 @@ def main():
     print(True, equilateral([test1]))
     print(False, equilateral([test2]))
     print(False, equilateral([test3]))
-    # print(False, isosceles([test1]))
+    # print(True, isosceles([test1]))
     # print(True, isosceles([test2]))
     # print(False, isosceles([test3]))
     # print(False, scalene([test1]))
