9eb64814f5f1fe991aa333ee82dd2d5a91e53ce2
GytisMalinauskas <pvardenis30@gmail.com>
2026-09-15 14:20:23 +0000

    Sep 15, 2026, 5:20 PM

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -12,7 +12,7 @@ def isosceles(sides):
     """Checks if the triangle is isoceles and returns boolean value"""
     if not is_triangle(sides):
         return False
-    if not equilateral and not scalene:
+    if equilateral and not scalene:
         return True
     return False
 
@@ -21,7 +21,7 @@ def scalene(sides):
     if not is_triangle(sides):
         return False
     for index in range(len(sides) - 1):
-        if sides[index] == sides[(index + 1) % 3] or not sides[index] == sides[(index + 1) % 3]:
+        if sides[index] == sides[(index + 1) % 3] or sides[index] == sides[(index + 1) % 3]:
             return False
     return True
 
