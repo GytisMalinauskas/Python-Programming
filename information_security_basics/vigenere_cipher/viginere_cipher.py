5faaa78176c28ca87de3f59bccf5a658f44ff2a1
GytisMalinauskas <pvardenis30@gmail.com>
2026-09-22 18:38:49 +0000

    Sep 22, 2026, 9:38 PM

--- information_security_basics/vigenere_cipher/viginere_cipher.py
+++ information_security_basics/vigenere_cipher/viginere_cipher.py
@@ -145,9 +145,11 @@ def viginere_cipher():
         letters.append(char)
     alphabet_matrix = []
     for i in range(len(LT_ALPHABET)):
+        print(i, letters)
         alphabet_matrix.append(letters)
-        letters.pop()
-    return letters
+        value_to_move_back = letters.pop(0)
+        letters.append(value_to_move_back)
+    return ...
 
 def main():
     print(viginere_cipher())
